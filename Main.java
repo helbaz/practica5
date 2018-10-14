9cbd6aa5e7f0b253287688eaecf938497af89a7f
hamza <helbaz1993@gmail.com>
2018-10-14 23:17:11 +0200

    primer commit en temps

--- Main.java
+++ Main.java
@@ -4,21 +4,26 @@ import java.util.Scanner;
 public class Main {
    public static void main(String[] args) {
        Scanner teclado=new Scanner(System.in);
-       int[] array=new int[50000];
+       int[] array=new int[1000000];
        Random random=new Random();
        for (int i = 0; i < array.length; i++) {
-           array[i]=random.nextInt(50000);
+           array[i]=random.nextInt(array.lenght);
        }
+
        System.out.println("Dime un numero");
        int num=teclado.nextInt();
        boolean esta=false;
+       long empieza=System.currentTimeMillis();
        for (int i = 0; i < array.length; i++) {
            if (array[i]==num){
                esta=true;
            }
        }
-       if (esta) System.out.println("Esta en la lista");
-       else System.out.println("No esta en la lista");
+       long termina=System.currentTimeMillis();
+       if (esta) System.out.println("Esta en la lista "+(empieza-termina));
+       else System.out.println("No esta en la lista "+(empieza-termina));
    }
 }
+
+
 
